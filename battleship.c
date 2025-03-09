6fa63160cbd9056a46e6a14bf7860a006d7c5d8e
Gabriel Antunes <gabrielantunes7711@gmail.com>
2025-03-09 16:39:40 -0300

    Feat: Posiciona barcos da diagonal e define uma constante para o tabuleiro

--- battleship.c
+++ battleship.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+#define BOARD_COLUMNS 10
+#define BOARD_ROWS 10
+
 int main() {
   // Cria o tabuleiro
-  int board[10][10];
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
+  int board[BOARD_ROWS][BOARD_COLUMNS];
+  for (int i = 0; i < BOARD_ROWS; i++) {
+    for (int j = 0; j < BOARD_COLUMNS; j++) {
       board[i][j] = 0;
     }
   }
@@ -18,20 +21,28 @@ int main() {
     board[6][i + 4] = 3;
   }
 
+  for (int i = 0; i < 3; i++) {
+    board[7 + i][7 + i] = 3;
+  }
+
+  for (int i = 0; i < 3; i++) {
+    board[1 + i][8 - i] = 3;
+  }
+
   //Exibe o tabuleiro
   char row = 'A';
   printf("  ");
-  for (int i = 1; i <= 10; i++)
+  for (int i = 1; i <= BOARD_COLUMNS; i++)
   {
     printf("%c ", row);
     row++;
   }
   printf("\n");
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < BOARD_ROWS; i++) {
     printf("%d ", i);
 
-    for (int j = 0; j < 10; j++) {
+    for (int j = 0; j < BOARD_COLUMNS; j++) {
       printf("%d ", board[i][j]);
     }
 
